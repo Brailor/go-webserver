87772c4f8e9592f810aff483214f3f38b4955393
Viktor Ohad <viktor.ohad@instructure.com>
2021-09-13 20:49:05 +0200

    add basic terraform setup for: ecr, ecs, vpc, and public subnet"

--- main.tf
+++ main.tf
@@ -9,31 +9,140 @@ terraform {
   required_version = ">= 0.14.9"
 }
 
+variable "cidr" {
+  description = "The CIDR block for the VPC."
+  default     = "10.0.0.0/16"
+}
+variable "public_subnets" {
+  description = "a list of CIDRs for public subnets in your VPC, must be set if the cidr variable is defined, needs to have as many elements as there are availability zones"
+  default     = ["10.0.16.0/20", "10.0.48.0/20", "10.0.80.0/20"]
+}
+
+variable "availability_zones" {
+  description = "a comma-separated list of availability zones, defaults to all AZ of the region, if set to something other than the defaults, both private_subnets and public_subnets have to be defined as well"
+  default     = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+}
+
 provider "aws" {
   profile = "default"
   region  = "eu-central-1"
 }
 
-resource "aws_instance" "go_server" {
-  ami           = "ami-07df274a488ca9195"
-  instance_type = "t2.micro"
-  user_data     = file("./app.yml")
-
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
+resource "aws_vpc" "main" {
+  cidr_block = var.cidr
+}
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
 }
 
-resource "aws_security_group" "web-sg" {
-  name = "my-sg"
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = element(var.public_subnets, count.index)
+  availability_zone       = element(var.availability_zones, count.index)
+  count                   = length(var.public_subnets)
+  map_public_ip_on_launch = true
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnets)
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_security_group" "ecs_tasks" {
+  name   = "brailor-sg-task"
+  vpc_id = aws_vpc.main.id
+
   ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    protocol         = "tcp"
+    from_port        = 80
+    to_port          = 80
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    protocol         = "-1"
+    from_port        = 0
+    to_port          = 0
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+resource "aws_ecr_repository" "brailor_ecr" {
+  name = "brailor_ecr"
+}
+
+resource "aws_ecs_cluster" "brailor_ecs" {
+  name = "brailor_ecs"
+}
+
+resource "aws_ecs_task_definition" "run-on-init" {
+  family                   = "run-on-init"
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "run-on-init",
+      "image": "${aws_ecr_repository.brailor_ecr.repository_url}",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 80,
+          "hostPort": 80
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
+  network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
+  memory                   = 512         # Specifying the memory our container requires
+  cpu                      = 256         # Specifying the CPU our container requires
+  execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
+}
+
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
   }
 }
 
-output "web-address" {
-  value = "${aws_instance.go_server.public_dns}:8080"
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = aws_iam_role.ecsTaskExecutionRole.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_ecs_service" "go-webserver" {
+  name            = "go-webserver"
+  cluster         = aws_ecs_cluster.brailor_ecs.id
+  task_definition = aws_ecs_task_definition.run-on-init.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+  network_configuration {
+    security_groups  = [aws_security_group.ecs_tasks.id]
+    subnets          = aws_subnet.public.*.id
+    assign_public_ip = true
+  }
 }
